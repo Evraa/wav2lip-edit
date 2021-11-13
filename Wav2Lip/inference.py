97a3e5626145da8412db062c63d919071548b3d1
Evraa <evramyousef@gmail.com>
2021-11-13 13:34:30 +0000

    nosmooth

--- Wav2Lip/inference.py
+++ Wav2Lip/inference.py
@@ -52,7 +52,7 @@ parser.add_argument('--nosmooth', default=False, action='store_true',
 
 args = parser.parse_args()
 args.img_size = 96
-
+print(f"nosmooth: {args.nosmooth}")
 if os.path.isfile(args.face) and args.face.split('.')[1] in ['jpg', 'png', 'jpeg']:
 	args.static = True
 
